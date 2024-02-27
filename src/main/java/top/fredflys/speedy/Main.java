d7b941622e8d1a3e9d6ac9caf8f34a13ce38aac8
fredflys <xyf220@126.com>
2024-02-27 13:28:37 -0800

    added basic download function

--- src/main/java/top/fredflys/speedy/Main.java
+++ src/main/java/top/fredflys/speedy/Main.java
@@ -2,10 +2,13 @@ package top.fredflys.speedy;
 
 import java.nio.file.Path;
 
+import top.fredflys.speedy.core.Downloader;
 import top.fredflys.speedy.util.Utils;
 
 public class Main {
     public static void main(String[] args) {
         Path outputPath = Utils.parseArgs(args);
+        Downloader downloader = new Downloader();
+        downloader.download(args[0], outputPath);
     }
 }
--- src/main/java/top/fredflys/speedy/core/Downloader.java
+++ src/main/java/top/fredflys/speedy/core/Downloader.java
@@ -1,21 +1,49 @@
 package top.fredflys.speedy.core;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Path;
 
 import top.fredflys.speedy.constant.Constants;
 
 public class Downloader {
     
-    static HttpURLConnection connect(String url) throws IOException   {
+    private HttpURLConnection connect(String url) throws IOException   {
         URL target = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) target.openConnection();
         connection.addRequestProperty("User-Agent", Constants.USER_AGENT);
         return connection;
     }
-    
-    static void download(String url) {
-        
+
+    public void download(String url, Path outputPath) {
+        HttpURLConnection connection = null;
+        try {
+           connection = connect(url);
+        } catch (IOException e) {
+            System.err.println("Error: " + e);
+        }
+
+        try (
+            BufferedInputStream bufferIn = new BufferedInputStream(
+                connection.getInputStream()
+            );
+            BufferedOutputStream bufferOut = new BufferedOutputStream(
+                new FileOutputStream(outputPath.toString())
+            );
+        ) {
+            int len = -1;
+            while ((len = bufferIn.read()) != -1) {
+                bufferOut.write(len);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e);
+        } catch (Exception e) {
+            System.err.println("Error: " + e);;
+        }
     }
 }
