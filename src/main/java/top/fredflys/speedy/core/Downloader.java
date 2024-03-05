c8b48007b6fb7e956adb603840d8fa89362ce278
fredflys <xyf220@126.com>
2024-03-05 13:40:01 -0800

    added a thread to run analyzer

--- src/main/java/top/fredflys/speedy/core/Downloader.java
+++ src/main/java/top/fredflys/speedy/core/Downloader.java
@@ -8,12 +8,17 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import top.fredflys.speedy.constant.Constants;
 import top.fredflys.speedy.util.Log;
+import top.fredflys.speedy.util.Utils;
 
 public class Downloader {
-    
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
     private HttpURLConnection connect(String url) throws IOException   {
         URL target = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) target.openConnection();
@@ -23,11 +28,29 @@ public class Downloader {
 
     public void download(String url, Path outputPath) {
         HttpURLConnection connection = null;
+        long resourceSize = 0;
         try {
            connection = connect(url);
+           resourceSize = connection.getContentLengthLong();
+
+           if (resourceSize <= 0) {
+               Log.error("The url is invalid at %s. Aborted.", url);
+           }
         } catch (IOException e) {
-            System.err.println("Error: " + e);
+            Log.error(e.toString());
         }
+
+        long localFileSize = Utils.getFileSize(outputPath.toString());
+        if (localFileSize == -1) {
+            Log.error("%s is not a valid path for the output file.", outputPath.toString());
+        }
+        if (localFileSize >= resourceSize) {
+            Log.info("The target url has been downloaded to %s.", outputPath.toString());
+            System.exit(0);
+        }
+
+        Analyzer analyzer = new Analyzer(resourceSize, localFileSize);
+        scheduler.scheduleAtFixedRate(analyzer, 1, 1, TimeUnit.SECONDS);
 
         try (
             BufferedInputStream bufferIn = new BufferedInputStream(
@@ -38,14 +61,20 @@ public class Downloader {
             );
         ) {
             int len = -1;
-            while ((len = bufferIn.read()) != -1) {
-                bufferOut.write(len);
+            byte[] buffer = new byte[Constants.DOWNLOAD_BUFFER_SIZE];
+            while ((len = bufferIn.read(buffer)) != -1) {
+                bufferOut.write(buffer, 0, len);
+                analyzer.setDownloadedInBytes(analyzer.getDownloadedInBytes() + len);
             }
         } catch (FileNotFoundException e) {
             Log.error("File not found: %s", e.toString());
         } catch (Exception e) {
             System.err.println("Error: " + e);
             Log.error(e.toString());
+        } finally {
+            if (connection != null) connection.disconnect();
+            scheduler.shutdownNow();
+            System.out.printf("\r\rDownload finished. File is saved to %s.", outputPath.toString());
         }
     }
 }
